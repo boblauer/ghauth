06082bd48819b1d476d41025aa6225412b1074a7
Bob Lauer <rlauer@gmail.com>
2017-03-02 11:18:20 -0600

    testing reading heroku var

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var express = require('express');
+var request = require('request');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
+app.get('/', (req, res) => {
+  res.send(process.env.test);
 });
 
 app.listen(app.get('port'), () => {
