808d729eec785eb1a45a92e6e6fe4fee202188a3
Bob Lauer <rlauer@gmail.com>
2017-03-14 14:49:35 -0500

    pubsub

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 'use strict';
 
-var express = require('express');
-var request = require('request');
-var querystring = require('querystring');
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-var app = express();
+var request = require('request');
+var bodyParser = require('body-parser');
+var xhub = require('express-x-hub');
+var querystring = require('querystring');
 
 var clientId = 'cd02dc9c9a0783ffb9f7';
 var clientSecret = process.env.ghsecret;
+var webHookSecret = process.env.ghsecret;
+
+io.sockets.on('connection', (socket) => {
+  console.log('socket connected!');
+  socket.on('room', (room) => {
+    console.log('socked joined room', room);
+    socket.join(room);
+  });
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -32,6 +44,39 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(app.get('port'), () => {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.post('/subscribe', bodyParser.json(), (req, res) => {
+  let repo = req.body.repo;
+  let token = req.body.token;
+
+  var fullUrl = req.protocol + '://' + req.get('host') + '/hook';
+
+  request.post({
+    url: `https://api.github.com/repos/${repo}/hooks`,
+    headers: {
+      Authorization: `token ${token}`,
+      'User-Agent': 'User boblauer, Application GitHub Event Stream'
+    },
+    json: {
+      name: 'web',
+      active: true,
+      events: ['*'],
+      config: {
+        url: fullUrl,
+        content_type: 'json',
+        secret: webHookSecret
+      }
+    }
+  }).pipe(res);
 });
+
+app.post('/hook', xhub({ algorithm: 'sha1', secret: webHookSecret }), (req, res) => {
+  if (!req.isXHubValid()) return res.status(404).send();
+
+  var room = 'ifit/ifit'; // get room from req object
+  var payload = { data: true }; // get data from req object.
+  io.sockets.in(room).emit('message', payload);
+
+  res.status(204).send();
+});
+
+server.listen(app.get('port'), () => console.log(`Node app is running on port: ${app.get('port')}`));
